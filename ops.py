11f36948a7624d5b235775d4f8f5d2a477f2696b
akhil kumar konda <akhilkumarco007@gmail.com>
2018-11-26 16:27:34 -0600

    Update ops.py

--- ops.py
+++ ops.py
@@ -2,13 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 
-# weight and bais wrappers
 def weight_variable(shape):
-    """
-    Create a weight variable with appropriate initialization
-    :param shape: weight shape
-    :return: initialized weight variable
-    """
     initer = tf.truncated_normal_initializer(stddev=0.01)
     return tf.get_variable('W',
                            dtype=tf.float32,
@@ -17,11 +11,6 @@ def weight_variable(shape):
 
 
 def bias_variable(shape):
-    """
-    Create a bias variable with appropriate initialization
-    :param shape: bias variable shape
-    :return: initialized bias variable
-    """
     initer = tf.constant(0., shape=shape, dtype=tf.float32)
     return tf.get_variable('b',
                            dtype=tf.float32,
@@ -29,12 +18,6 @@ def bias_variable(shape):
 
 
 def LSTM(x, weights, biases, num_hidden, seqLen):
-    """
-    :param x: inputs of size [T, batch_size, input_size]
-    :param weights: matrix of fully-connected output layer weights
-    :param biases: vector of fully-connected output layer biases
-    :param num_hidden: number of hidden units
-    """
     multi_lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(num_hidden) for _ in range(3)])
     outputs, states = tf.nn.dynamic_rnn(multi_lstm_cell, x, sequence_length=seqLen, dtype=tf.float32)
     num_examples = tf.shape(x)[0]
